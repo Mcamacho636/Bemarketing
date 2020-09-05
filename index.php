9516acfe9ad38fd547dbd2fdb3c8e1509fd0886c
Miguel <mcamacho3@ucol.mx>
2020-09-05 14:03:26 -0500

    ajustes en la estructura

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <!--INCLUIMOS BOOSTRAP 4.5 EN EL PROYECTO-->
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap/w3.css">
+    <link rel="stylesheet" href="css/empresas.css">
+    <link rel="stylesheet" href="css/styles.css ">
+    <title>BeMarketing</title>
+
+    <script src="scripts/bootstrap.min.js"></script>
+    <script src="scripts/jquery-3.5.1.slim.min.js"></script>
+    <script src="scripts/popper.min.js"></script>
+    <script src="scripts/navbar.js"></script>
+</head>
+
+<body>
+    <!--NavBar-->
+    <?php require("templates/navbar.php");?>
+    <!--Contenido-->
+    <?php require("pages/index.php");?>
+</body>
+
+</html>
---
+++ pages/index.php
@@ -0,0 +1,133 @@
+<!--Carrusel-->
+<header>
+    <div class="contenedor-carrusel ">
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="Assets/Imagen1.jpeg" class="d-block w-100" alt="BeMarketing1">
+                </div>
+                <div class="carousel-item">
+                    <img src="Assets/Imagen2.jpeg" class="d-block w-100" alt="BeMarketing2">
+                </div>
+                <div class="carousel-item">
+                    <img src="Assets/Imagen3.jpg" class="d-block w-100" alt="BeMarketing3">
+                </div>
+            </div>
+        </div>
+        <div class="bottomleft ">
+            <p>BeMarketing</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+    </div>
+</header>
+
+<!--SECCIÓN DE MÁS SOBRE NOSOTROS-->
+<section class="seccion ">
+    <h2 class="mayusculas centrar-texto ">m&aacute;s sobre nosotros</h2>
+    <div class="contenedor contenido-mas-nosotros ">
+        <div class="card ">
+            <img src="Assets/Imagen1.jpeg " class="card-img-top " alt="... ">
+            <div class="card-body ">
+                <p class="card-text ">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas ut dignissimos ullam magni ipsam odit dicta beatae similique quos obcaecati officia, id nesciunt vel saepe ducimus, incidunt, in sint ea.</p>
+            </div>
+        </div>
+        <div class="card ">
+            <img src="Assets/Imagen1.jpeg " class="card-img-top " alt="... ">
+            <div class="card-body ">
+                <p class="card-text ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi assumenda eveniet odio repudiandae consequuntur, sunt perspiciatis autem eius, impedit cupiditate tenetur veritatis? Labore, sunt. Quas dicta et unde beatae pariatur.</p>
+            </div>
+        </div>
+        <div class="card ">
+            <img src="Assets/Imagen1.jpeg " class="card-img-top " alt="... ">
+            <div class="card-body ">
+                <p class="card-text ">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim assumenda reprehenderit labore commodi aliquid expedita, similique impedit eaque mollitia omnis tempore, voluptates odit voluptate </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--SECCION DE TRABAJOS-->
+<seccion class="seccion ">
+    <h2 class="centrar-texto mayusculas ">trabajos</h2>
+    <div class="contenedor contenido-trabajos ">
+        <div class="w3-card-4">
+            <img src="Assets/Imagen2.jpeg " alt="Alps " style="width:100% ">
+            <div class="w3-container w3-center ">
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <a href="#" class="button-trabajos">Ver Trabajo</a>
+        </div>
+        <div class="w3-card-4">
+            <img src="Assets/Imagen2.jpeg " alt="Alps " style="width:100% ">
+            <div class="w3-container w3-center ">
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <a href="#" class="button-trabajos">Ver Trabajo</a>
+        </div>
+        <div class="w3-card-4">
+            <img src="Assets/Imagen2.jpeg " alt="Alps " style="width:100% ">
+            <div class="w3-container w3-center ">
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <a href="#" class="button-trabajos">Ver Trabajo</a>
+        </div>
+    </div>
+</seccion>
+
+<!--CASOS DE EXITO-->
+<section class="seccion">
+    <h2 class="centrar-texto mayusculas">casos de &eacute;xito</h2>
+    <div class="carrusel-infinito">
+        <div class="slider">
+            <div class="slide-track">
+                <div class="slide">
+                    <img src="Assets/Imagen1.jpeg" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="Assets/Imagen1.jpeg" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+                </div>
+                <div class="slide">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
