fcac0d0d53de3277ae7cff4a652274c524e50ff2
Sebastian Ochoa Hiriart <saboochoahiriart@outlook.com>
2020-09-11 13:41:17 -0500

    Implementación de botón ir arriba y ocultar nav

--- nosotros.php
+++ nosotros.php
@@ -4,27 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-   
+
     <link rel="stylesheet" href="CSS/normalize.css">
-    <link rel="stylesheet" href="CSS/carrusel.css">
     <link rel="stylesheet" href="CSS/empresas.css">
     <link rel="stylesheet" href="CSS/styles.css">
     <title>BeMarketing</title>
     
     <script src="Scripts/jquery-3.5.1.js"></script>
+    
     <script src="Scripts/nav.js"></script>
-    <script src="Scripts/servicios.js"></script>
-    <script src="Scripts/carrusel.js"></script>
-    <script src="Scripts/carrusel_automatico.js"></script>
 </head>
 
-<body onload="currentSlide(3)">
-
+<body>
     <!--NavBar-->
     <?php require("templates/ir-arriba.php");?>
     <?php require("templates/nav.php");?>
-    <?php require("templates/carrusel.php");?>
-    <?php require("pages/index.php");?>
+    <?php require("pages/nosotros.php");?>
     <?php require("templates/footer.php");?>
 </body>
 
---
+++ pages/nosotros.php
@@ -0,0 +1,118 @@
+<!--CONTENIDO ACERCA DE-->
+<main class="Contenedor-Seccion Contenido-Acerca" style="margin-top:0;">
+        <div class="texto-acerca-de">
+            <h1 class="Centrar-Texto Texto-Mayusculas">Acerca de nosotros</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dapibus ipsum in mauris placerat, nec dapibus orci venenatis. Suspendisse ut nisl in massa pharetra auctor ut non lectus. Aliquam erat volutpat. Etiam ac elit finibus est laoreet
+                tempus. In tincidunt vestibulum erat, sit amet finibus ipsum lobortis sed. Nam id mi in felis auctor luctus. In dignissim at erat eu mollis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Interdum et malesuada
+                fames ac ante ipsum primis in faucibus. Suspendisse egestas dui at dolor suscipit, vitae sollicitudin est placerat. Quisque vel felis a ipsum pulvinar commodo non vitae est. Donec ut erat eget ligula volutpat dapibus.</p>
+            <p class="oculto-responsivo">1Aliquam erat volutpat. Praesent tortor eros, ultricies eu erat non, bibendum lacinia nibh. Vestibulum vel massa at massa sollicitudin dignissim quis ut odio. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+                mus. Etiam ac rhoncus nulla. Mauris non velit molestie, molestie dolor non, viverra neque. Mauris lacinia, orci sit amet porta consequat, ex dui luctus arcu, et tempus orci felis at velit. Curabitur a feugiat arcu. Curabitur eu neque sem.
+                Quisque condimentum id tortor a blandit. Vestibulum cursus tellus blandit enim luctus, vel ultricies enim scelerisque. Aenean mollis sodales congue.</p>
+        </div>
+        <img src="Assets/Imagen4.jpg" alt="">
+    </main>
+    <section class="Contenedor-Seccion">
+        <div class="Contenido-Nosotros">
+            <div class="Tarjeta-Simple">
+                <img src="Assets/Imagen1.jpeg " class="" alt="... ">
+                <div class="Contenido-Tarjeta-Simple">
+                    <h3>Lorem</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas ut dignissimos ullam magni ipsam odit dicta beatae similique quos obcaecati officia, id nesciunt vel saepe ducimus, incidunt, in sint ea.</p>
+                </div>
+            </div>
+            <div class="Tarjeta-Simple">
+                <img src="Assets/Imagen1.jpeg " class="" alt="... ">
+                <div class="Contenido-Tarjeta-Simple">
+                    <h3>Lorem</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas ut dignissimos ullam magni ipsam odit dicta beatae similique quos obcaecati officia, id nesciunt vel saepe ducimus, incidunt, in sint ea.</p>
+                </div>
+            </div>
+            <div class="Tarjeta-Simple">
+                <img src="Assets/Imagen1.jpeg " class="" alt="... ">
+                <div class="Contenido-Tarjeta-Simple">
+                    <h3>Lorem</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas ut dignissimos ullam magni ipsam odit dicta beatae similique quos obcaecati officia, id nesciunt vel saepe ducimus, incidunt, in sint ea.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="Contenedor-Seccion">
+        <div class="Carrusel-Infinito" style="height: auto;">
+            <div class="slider" style="height: auto;">
+                <div class="slide-track" style="height: auto;">
+                    <div class="Tarjeta-Simple tarjeta-equipo">
+                        <img src="Assets/Imagen1.jpeg " class="" alt="... ">
+                        <div class="Contenido-Tarjeta-Simple">
+                            <h3>Lorem</h3>
+                        </div>
+                    </div>
+                    <div class="Tarjeta-Simple tarjeta-equipo">
+                        <img src="Assets/Imagen1.jpeg " class="" alt="... ">
+                        <div class="Contenido-Tarjeta-Simple">
+                            <h3>Lorem</h3>
+                        </div>
+                    </div>
+                    <div class="Tarjeta-Simple tarjeta-equipo">
+                        <img src="Assets/Imagen1.jpeg " class="" alt="... ">
+                        <div class="Contenido-Tarjeta-Simple">
+                            <h3>Lorem</h3>
+                        </div>
+                    </div>
+                    <div class="Tarjeta-Simple tarjeta-equipo">
+                        <img src="Assets/Imagen1.jpeg " class="" alt="... ">
+                        <div class="Contenido-Tarjeta-Simple">
+                            <h3>Lorem</h3>
+                        </div>
+                    </div>
+                    <div class="Tarjeta-Simple tarjeta-equipo">
+                        <img src="Assets/Imagen1.jpeg " class="" alt="... ">
+                        <div class="Contenido-Tarjeta-Simple">
+                            <h3>Lorem</h3>
+                        </div>
+                    </div>
+                    <div class="Tarjeta-Simple tarjeta-equipo">
+                        <img src="Assets/Imagen1.jpeg " class="" alt="... ">
+                        <div class="Contenido-Tarjeta-Simple">
+                            <h3>Lorem</h3>
+                        </div>
+                    </div>
+                    <div class="Tarjeta-Simple tarjeta-equipo">
+                        <img src="Assets/Imagen1.jpeg " class="" alt="... ">
+                        <div class="Contenido-Tarjeta-Simple">
+                            <h3>Lorem</h3>
+                        </div>
+                    </div>
+                    <div class="Tarjeta-Simple tarjeta-equipo">
+                        <img src="Assets/Imagen1.jpeg " class="" alt="... ">
+                        <div class="Contenido-Tarjeta-Simple">
+                            <h3>Lorem</h3>
+                        </div>
+                    </div>
+                    <div class="Tarjeta-Simple tarjeta-equipo">
+                        <img src="Assets/Imagen1.jpeg " class="" alt="... ">
+                        <div class="Contenido-Tarjeta-Simple">
+                            <h3>Lorem</h3>
+                        </div>
+                    </div>
+                    <div class="Tarjeta-Simple tarjeta-equipo">
+                        <img src="Assets/Imagen1.jpeg " class="" alt="... ">
+                        <div class="Contenido-Tarjeta-Simple">
+                            <h3>Lorem</h3>
+                        </div>
+                    </div>
+                    <div class="Tarjeta-Simple tarjeta-equipo">
+                        <img src="Assets/Imagen1.jpeg " class="" alt="... ">
+                        <div class="Contenido-Tarjeta-Simple">
+                            <h3>Lorem</h3>
+                        </div>
+                    </div>
+                    <div class="Tarjeta-Simple tarjeta-equipo">
+                        <img src="Assets/Imagen1.jpeg " class="" alt="... ">
+                        <div class="Contenido-Tarjeta-Simple">
+                            <h3>Lorem</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
