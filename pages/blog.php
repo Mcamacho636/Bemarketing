1f265dde6582515bd304cc11722f0def107207f1
Sebastian Ochoa Hiriart <saboochoahiriart@outlook.com>
2020-09-18 14:00:23 -0500

    Se agregó secciónd de Blog

---
+++ pages/blog.php
@@ -0,0 +1,133 @@
+<section class="Seccion-Contenedor Contenido-Portafolio">
+    <img src="Assets/Imagen4.jpg" alt="">
+    <div class="text-block">
+        <h1>NUESTRO PORTAFOLIO</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia placeat rerum consectetur fugit, tempore
+            illum nobis facilis reiciendis!</p>
+    </div>
+</section>
+<main class="Contenedor-Seccion Blog-Blog">
+    <a href="">
+        <div class="Tarjeta-Blog-Ext">
+            <img src="Assets/Imagen3.jpg" alt="">
+            <p class="Blog-Categoria">Categoria</p>
+            <div class="Contenido-Tarjeta-Blog-Ext">
+                <h2 class="Centrar-Texto">Titulo Blog</h2>
+                <h4 class="Centrar-Texto">--/--/---- by Admin</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam fuga perferendis fugiat ea hic
+                    alias
+                    quibusdam eum aperiam magnam architecto consequatur ratione placeat ad tenetur ipsum aut, nam neque
+                    labore.</p>
+            </div>
+        </div>
+    </a>
+    <a href="">
+        <div class="Tarjeta-Blog-Ext">
+            <img src="Assets/Imagen3.jpg" alt="">
+            <p class="Blog-Categoria">Categoria</p>
+            <div class="Contenido-Tarjeta-Blog-Ext">
+                <h2 class="Centrar-Texto">Titulo Blog</h2>
+                <h4 class="Centrar-Texto">--/--/---- by Admin</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam fuga perferendis fugiat ea hic
+                    alias
+                    quibusdam eum aperiam magnam architecto consequatur ratione placeat ad tenetur ipsum aut, nam neque
+                    labore.</p>
+            </div>
+        </div>
+        </a>
+        <a href="">
+            <div class="Tarjeta-Blog-Ext">
+                <img src="Assets/Imagen3.jpg" alt="">
+                <p class="Blog-Categoria">Categoria</p>
+                <div class="Contenido-Tarjeta-Blog-Ext">
+                    <h2 class="Centrar-Texto">Titulo Blog</h2>
+                    <h4 class="Centrar-Texto">--/--/---- by Admin</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam fuga perferendis fugiat ea
+                        hic
+                        alias
+                        quibusdam eum aperiam magnam architecto consequatur ratione placeat ad tenetur ipsum aut, nam
+                        neque
+                        labore.</p>
+                </div>
+            </div>
+        </a>
+        <a href="">
+            <div class="Tarjeta-Blog-Ext">
+                <img src="Assets/Imagen3.jpg" alt="">
+                <p class="Blog-Categoria">Categoria</p>
+                <div class="Contenido-Tarjeta-Blog-Ext">
+                    <h2 class="Centrar-Texto">Titulo Blog</h2>
+                    <h4 class="Centrar-Texto">--/--/---- by Admin</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam fuga perferendis fugiat ea
+                        hic
+                        alias
+                        quibusdam eum aperiam magnam architecto consequatur ratione placeat ad tenetur ipsum aut, nam
+                        neque
+                        labore.</p>
+                </div>
+            </div>
+        </a>
+        <a href="">
+            <div class="Tarjeta-Blog-Ext">
+                <img src="Assets/Imagen3.jpg" alt="">
+                <p class="Blog-Categoria">Categoria</p>
+                <div class="Contenido-Tarjeta-Blog-Ext">
+                    <h2 class="Centrar-Texto">Titulo Blog</h2>
+                    <h4 class="Centrar-Texto">--/--/---- by Admin</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam fuga perferendis fugiat ea
+                        hic
+                        alias
+                        quibusdam eum aperiam magnam architecto consequatur ratione placeat ad tenetur ipsum aut, nam
+                        neque
+                        labore.</p>
+                </div>
+            </div>
+        </a>
+        <a href="">
+            <div class="Tarjeta-Blog-Ext">
+                <img src="Assets/Imagen3.jpg" alt="">
+                <p class="Blog-Categoria">Categoria</p>
+                <div class="Contenido-Tarjeta-Blog-Ext">
+                    <h2 class="Centrar-Texto">Titulo Blog</h2>
+                    <h4 class="Centrar-Texto">--/--/---- by Admin</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam fuga perferendis fugiat ea
+                        hic
+                        alias
+                        quibusdam eum aperiam magnam architecto consequatur ratione placeat ad tenetur ipsum aut, nam
+                        neque
+                        labore.</p>
+                </div>
+            </div>
+        </a><a href="">
+            <div class="Tarjeta-Blog-Ext">
+                <img src="Assets/Imagen3.jpg" alt="">
+                <p class="Blog-Categoria">Categoria</p>
+                <div class="Contenido-Tarjeta-Blog-Ext">
+                    <h2 class="Centrar-Texto">Titulo Blog</h2>
+                    <h4 class="Centrar-Texto">--/--/---- by Admin</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam fuga perferendis fugiat ea
+                        hic
+                        alias
+                        quibusdam eum aperiam magnam architecto consequatur ratione placeat ad tenetur ipsum aut, nam
+                        neque
+                        labore.</p>
+                </div>
+            </div>
+        </a>
+        <a href="">
+            <div class="Tarjeta-Blog-Ext">
+                <img src="Assets/Imagen3.jpg" alt="">
+                <p class="Blog-Categoria">Categoria</p>
+                <div class="Contenido-Tarjeta-Blog-Ext">
+                    <h2 class="Centrar-Texto">Titulo Blog</h2>
+                    <h4 class="Centrar-Texto">--/--/---- by Admin</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam fuga perferendis fugiat ea
+                        hic
+                        alias
+                        quibusdam eum aperiam magnam architecto consequatur ratione placeat ad tenetur ipsum aut, nam
+                        neque
+                        labore.</p>
+                </div>
+            </div>
+        </a>
+</main>
--- templates/nav.php
+++ templates/nav.php
@@ -3,7 +3,7 @@
     <nav class="topnav" id="myTopnav">
         <div class="icono-nav">
             <a class="navbar-brand" href="index.php" style="color:#ffffff;">
-                <img src="" style="width: 20px; height: 20px;"> BeMarketing
+                <img src="" style="width: 32px; height: 32px;"> BeMarketing
             </a>
         </div>
         <div class="nav-enlaces">
@@ -11,10 +11,12 @@
             <a href="nosotros.php">Nosotros</a>
             <a href="portafolio.php">Portafolio</a>
             <a href="contacto.php">Contacto</a>
-            <a href="blog.html">Blog</a>
+            <a href="blog.php">Blog</a>
             <a href="javascript:void(0);" class="icon" onclick="desplegarMenu()">
-                <svg width="30px" height="30px" viewBox="0 0 16 16" class="bi bi-list" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                <svg width="30px" height="30px" viewBox="0 0 16 16" class="bi bi-list" fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd"
+                        d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                 </svg>
             </a>
         </div>
