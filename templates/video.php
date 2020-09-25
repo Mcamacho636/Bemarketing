dce8367a3768ad79ca08580c854986d4b90f2934
SaboHiriart <saboochoahiriart@outlook.com>
2020-09-25 16:00:56 -0500

    se agrego imagen preview al video y botón para volver a reproducir

--- templates/video.php
+++ templates/video.php
@@ -1,30 +1,51 @@
 
 <div id="hero">
-    <video autoplay preload="auto" id="Video_Inicio" poster="Assets/Imagen1.jpeg">
+    <video autoplay controls preload="auto" id="Video_Inicio" poster="Assets/Imagen1.jpeg">
         <source src="" type="video/mp4">
         Your browser does not support the video tag.  
     </video>
-
-    <div class="caption">
-        <img onclick="play();" src="Assets/play.png" style="width: 40px; height:40px;" >
+    <div class="d-none" id="imagen-fondo">
+        <img id="banner_video" alt="">  
+        <div class="centered">
+            <img src="Assets/play2.png" alt="" style="width=50px; height=50px;" onclick="play();">
+        </div>
     </div>
+    
 </div> 
 <script>
     
 var myVideo = document.getElementById("Video_Inicio");
-
-function play() {
-    if (myVideo.paused)
-        myVideo.play();
-    else
-        myVideo.pause();
-}
+var div_img = document.getElementById("imagen-fondo"); 
+var banner = document.getElementById("banner_video");
 
 function cambioSrc(){
     if(screen.width > 750){
         myVideo.setAttribute("src", "Assets/Video_Completo.m4v");
+        myVideo.setAttribute("poster", "Assets/BeDifferent_PC.png");
+        banner.setAttribute("src", "Assets/BeDifferent_PC.png");
+
     }else{
         myVideo.setAttribute("src", "Assets/Video_Responsive.m4v");
+        myVideo.setAttribute("poster", "Assets/BeDifferent_Responsive.png");
+        banner.setAttribute("src", "Assets/BeDifferent_Responsive.png");
     }
+
+    var pathname = window.location.pathname;
+    if (pathname === "/Repositorio%20BeMarketing/Bemarketing/index.php") {
+        $('#myNavbar').css("background-color", "transparent");
+    }
+}
+
+document.getElementById('Video_Inicio').addEventListener('ended', handlerVideo, false);
+function handlerVideo(e) {
+     myVideo.className= "d-none"; //Agregar clase
+        
+    div_img.className = ""; //Quitar clase
+}
+
+function play(){
+    myVideo.className = "";
+    div_img.className = "d-none";
+    myVideo.play();
 }
 </script>
