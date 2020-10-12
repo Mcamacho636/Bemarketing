206c5edcc3ff1859279d18689d2dc43b6a36b391
SaboHiriart <saboochoahiriart@outlook.com>
2020-10-12 09:41:24 -0500

    Se agregaron waves a la información

--- pages/nosotros.php
+++ pages/nosotros.php
@@ -1,53 +1,53 @@
 <!--CONTENIDO ACERCA DE-->
-<main class="Contenedor-Seccion Contenido-Acerca" style="margin-top:60px">
-    <div class="texto-acerca-de">
-        <h1 class="Centrar-Texto Texto-Mayusculas animate__animated animate__backInDown" style="margin-top: 20px">Acerca
-            de nosotros</h1>
-        <p class="animate__animated animate__backInUp">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Curabitur dapibus ipsum in mauris placerat, nec dapibus orci venenatis. Suspendisse ut nisl in massa
-            pharetra auctor ut non lectus. Aliquam erat volutpat. Etiam ac elit finibus est laoreet
-            tempus. In tincidunt vestibulum erat, sit amet finibus ipsum lobortis sed. Nam id mi in felis auctor luctus.
-            In dignissim at erat eu mollis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi.
-            Interdum et malesuada
-            fames ac ante ipsum primis in faucibus. Suspendisse egestas dui at dolor suscipit, vitae sollicitudin est
-            placerat. Quisque vel felis a ipsum pulvinar commodo non vitae est. Donec ut erat eget ligula volutpat
-            dapibus.</p>
+<main class="Seccion-Contenedor Contenido-Portafolio">
+    <img src="Assets/Imagen4.jpg" alt="">
+    <div class="text-block">
+        <h1>NUESTRO PORTAFOLIO</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia placeat rerum consectetur fugit, tempore illum nobis facilis reiciendis!</p>
     </div>
-    <img src="Assets/Imagen4.jpg" alt="" style="border-radius: 50%;">
 </main>
 <section class="Contenedor-Seccion">
-    <div class="Contenido-Nosotros">
-        <div class="Tarjeta-Simple zoom">
-            <img src="Assets/Imagen1.jpeg " class="" alt="... ">
-            <div class="Contenido-Tarjeta-Simple">
-                <h3>Lorem</h3>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas ut dignissimos ullam magni ipsam
-                    odit dicta beatae similique quos obcaecati officia, id nesciunt vel saepe ducimus, incidunt, in sint
-                    ea.</p>
+    <div class="Contenido-Nosotros-General">
+        <div class="wave-container">
+            <div class="Contenedor-Nosotros">
+                <img src="Assets/Imagen4.jpg" alt="">
+                <div class="Contenedor-Nosotros-Texto">
+                    <h1>Historia</h1>
+                    <P>Hola Mundo</P>
+                </div>
             </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#FFC100" fill-opacity="1" d="M0,32L40,48C80,64,160,96,240,90.7C320,85,400,43,480,32C560,21,640,43,720,53.3C800,64,880,64,960,85.3C1040,107,1120,149,1200,144C1280,139,1360,85,1400,58.7L1440,32L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path>
+            </svg>
         </div>
-        <div class="Tarjeta-Simple zoom">
-            <img src="Assets/Imagen1.jpeg " class="" alt="... ">
-            <div class="Contenido-Tarjeta-Simple">
-                <h3>Lorem</h3>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas ut dignissimos ullam magni ipsam
-                    odit dicta beatae similique quos obcaecati officia, id nesciunt vel saepe ducimus, incidunt, in sint
-                    ea.</p>
+        <div class="wave-container">
+            <div class="Contenedor-Nosotros">
+                <img src="Assets/Imagen4.jpg" alt="">
+                <div class="Contenedor-Nosotros-Texto">
+                    <h1>Historia</h1>
+                    <P>Hola Mundo</P>
+                </div>
             </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#FFC100" fill-opacity="1" d="M0,32L40,48C80,64,160,96,240,90.7C320,85,400,43,480,32C560,21,640,43,720,53.3C800,64,880,64,960,85.3C1040,107,1120,149,1200,144C1280,139,1360,85,1400,58.7L1440,32L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path>
+            </svg>
         </div>
-        <div class="Tarjeta-Simple zoom">
-            <img src="Assets/Imagen1.jpeg " class="" alt="... ">
-            <div class="Contenido-Tarjeta-Simple">
-                <h3>Lorem</h3>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas ut dignissimos ullam magni ipsam
-                    odit dicta beatae similique quos obcaecati officia, id nesciunt vel saepe ducimus, incidunt, in sint
-                    ea.</p>
+        <div class="wave-container">
+            <div class="Contenedor-Nosotros">
+                <img src="Assets/Imagen4.jpg" alt="">
+                <div class="Contenedor-Nosotros-Texto">
+                    <h1>Historia</h1>
+                    <P>Hola Mundo</P>
+                </div>
             </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#FFC100" fill-opacity="1" d="M0,32L40,48C80,64,160,96,240,90.7C320,85,400,43,480,32C560,21,640,43,720,53.3C800,64,880,64,960,85.3C1040,107,1120,149,1200,144C1280,139,1360,85,1400,58.7L1440,32L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path>
+            </svg>
         </div>
     </div>
 </section>
 <section class="Contenedor-Seccion">
-    <h2 class="Centrar-Texto Texto-Mayusculas">Nuesto Equipo</h2>
+    <h2 class="Centrar-Texto Texto-Mayusculas">Nuestro Equipo</h2>
     <div class="Carrusel-Infinito" style="height: auto;">
         <div class="slider" style="height: auto;">
             <div class="slide-track" style="height: auto;">
